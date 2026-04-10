fcba677fa565a771bfb51da461acd9e3ed2eb0b3
Victor Yrman <vitiayrashevich1256@gmail.com>
2026-04-10 15:34:51 +0300

    feat: add fonts and colors

---
+++ constants/theme.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: "#3EF7B4",
+  secondary: "#50EAD3",
+  success: "#00FF90",
+  error: "#FF0005",
+  background: "#0B0E23",
+  warning: "#FF6E00",
+  text: "#FFFFFF",
+  light: "#FFFFFF",
+  dark: "#0B0E23",
+};
+
